1a965e331aa9422eef41edd16123d2aa8756630f
Andrew Neuer <spam@planetneuer.com>
2024-08-27 18:09:06 -0700

    Init Site

---
+++ script.js
@@ -0,0 +1,20 @@
+// Serach Engine Optimization
+<script type="application/ld+json">
+{
+  "@context": "https://schema.org",
+  "@type": "LocalBusiness",
+  "name": "On the Edge - Sharpening by Rhoadz",
+  "address": {
+    "@type": "PostalAddress",
+    "streetAddress": "123 Sharpening St",
+    "addressLocality": "Blade City",
+    "addressRegion": "AZ",
+    "postalCode": "12345"
+  },
+  "telephone": "(123) 456-7890",
+  "openingHours": "Mo,Tu,We,Th,Fr 09:00-18:00",
+  "image": "https://www.yourwebsite.com/images/On-the-Edge.jpg",
+  "description": "Professional knife and tool sharpening services in Blade City."
+}
+</script>
+
